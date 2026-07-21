5ffaa1dbe0f1ea5a6ec3488263e3369ead8614b5
samik930 <samikpodder396@gmail.com>
2026-07-21 13:06:37 +0530

    url updated

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -10,7 +10,7 @@ require("dotenv").config();
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: process.env.CLIENT_URL,
     credentials :  true
 }))
 
