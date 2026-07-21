cc9950dfa093634fa68bee743ca84abebf3404d7
samik930 <samikpodder396@gmail.com>
2026-07-21 12:59:42 +0530

    url updated

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -2,6 +2,7 @@ require("dotenv").config()
 const app = require("./app")
 const connectToDB = require("./config/db")
 connectToDB()
-app.listen(3000, () => {
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
     console.log("Server is running on port 3000")
 })
