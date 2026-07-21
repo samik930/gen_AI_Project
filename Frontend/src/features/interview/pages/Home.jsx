d97a2d9b59bf3a66253937cdbd8ae06ad09db2ec
samik930 <samikpodder396@gmail.com>
2026-07-21 13:31:24 +0530

    Home.jsx updated

--- Frontend/src/features/interview/pages/Home.jsx
+++ Frontend/src/features/interview/pages/Home.jsx
@@ -29,7 +29,7 @@ const Home = () => {
 
         if (fieldsProvided === 1 || fieldsProvided === 0) { alert('Debug: Report generation skipped. Please provide required fields'); return; } // Exit early without generating report }
         const data = await generateReport({ jobDescription, selfDescription, resumeFile })
-        navigate(`/interview/${data._id}`)
+        navigate(`/interview/${data.id}`)
     }
 
     if (loading) {
