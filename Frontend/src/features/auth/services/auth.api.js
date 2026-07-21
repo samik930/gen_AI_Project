29865a9e17f263da12923e13c8903c8bba7e9413
samik930 <samikpodder396@gmail.com>
2026-07-21 14:23:08 +0530

    CORS updated again

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: "https://gen-ai-project-5w4j.onrender.com",
     withCredentials: true
 })
 
